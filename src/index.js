43e9ac4557d13c6d67d9c7abe750c6d6fa4e58b7
PanAndrey <panadr09@gmail.com>
2021-02-18 01:04:44 +0400

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let strArr = str.split("");
+  let open = [];
+  let close = [];
+  let st = [];
+  bracketsConfig.forEach(element => {
+    open.push(element[0]);
+    close.push(element[1]);
+  });
+  for (let i = 0; i < strArr.length; i++) {
+    if (close.indexOf(strArr[i]) != -1) {
+      let brkt = open[close.indexOf(strArr[i])];
+      let lastSt = st[st.length - 1];
+      if (lastSt == brkt) {
+        st.pop();
+      } else {
+        st.push(strArr[i]);
+      }
+    } else {
+      st.push(strArr[i]);
+    }
+  }
+  return st.length == 0;
+};
